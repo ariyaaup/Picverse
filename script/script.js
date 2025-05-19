21fc7b6a0b865c889cb8a0164e862555955c6ba9
ariyaaup <ariyautama77@gmail.com>
2025-05-19 22:05:58 +0700

    ]

--- script/script.js
+++ script/script.js
@@ -1,9 +1,17 @@
-// hamburger menu script
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("nav-menu");
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("nav-menu");
+  const overlay = document.getElementById("overlay");
 
-hamburger.addEventListener("click", function () {
-  console.log("Hamburger clicked");
-  navMenu.classList.toggle("active");
+  hamburger.addEventListener("click", function () {
+    this.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+
+  overlay.addEventListener("click", function () {
+    this.classList.remove("active");
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
 });
-// ------------------------
